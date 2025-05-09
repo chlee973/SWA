c71b70d97719ece8a44e72db65a2ed79daeb185c
Changhae Lee <chlee973@kaist.ac.kr>
2025-05-09 16:01:49 +0900

    Copy bn_update function

--- train_swa.py
+++ train_swa.py
@@ -183,7 +183,8 @@ def main():
 
     for epoch in range(args.exploring_epochs):
         train_swa(train_loader, model, swa_model, criterion, swa_optimizer, epoch)
-        update_batchnorm(swa_model, train_loader)
+        # update_batchnorm(swa_model, train_loader)
+        bn_update(train_loader, swa_model)
         acc_model = util.validate(val_loader, test_loader, model, criterion)
         acc = util.validate(val_loader, test_loader, swa_model, criterion)
         best_acc= max(acc, best_acc)
@@ -335,6 +336,61 @@ def update_batchnorm(swa_model, train_loader):
     with torch.no_grad():
         for i, (input, _) in enumerate(train_loader):
             _ = swa_model(input)
+
+def _check_bn(module, flag):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        flag[0] = True
+
+
+def check_bn(model):
+    flag = [False]
+    model.apply(lambda module: _check_bn(module, flag))
+    return flag[0]
+
+
+def reset_bn(module):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        module.running_mean = torch.zeros_like(module.running_mean)
+        module.running_var = torch.ones_like(module.running_var)
+
+
+def _get_momenta(module, momenta):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        momenta[module] = module.momentum
+
+
+def _set_momenta(module, momenta):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        module.momentum = momenta[module]
+
+def bn_update(loader, model):
+    """
+        BatchNorm buffers update (if any).
+        Performs 1 epochs to estimate buffers average using train dataset.
+
+        :param loader: train dataset loader for buffers average estimation.
+        :param model: model being update
+        :return: None
+    """
+    if not check_bn(model):
+        return
+    model.train()
+    momenta = {}
+    model.apply(reset_bn)
+    model.apply(lambda module: _get_momenta(module, momenta))
+    n = 0
+    for input, _ in loader:
+        input_var = input.cuda()
+        b = input_var.data.size(0)
+
+        momentum = b / (n + b)
+        for module in momenta.keys():
+            module.momentum = momentum
+
+        model(input_var)
+        n += b
+
+    model.apply(lambda module: _set_momenta(module, momenta))
 
 if __name__ == '__main__':
     main()
